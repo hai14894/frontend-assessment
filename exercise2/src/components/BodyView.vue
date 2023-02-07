44cc586ed1ef8cf5ebeca320283efce4fe50374c
Hai Ho <hohoanghai14894@gmail.com>
2023-02-08 10:21:31 +1100

    Add accordion view on mobile

--- exercise2/src/components/BodyView.vue
+++ exercise2/src/components/BodyView.vue
@@ -1,17 +1,32 @@
 <template>
-    <div>
-    <div 
-      v-if="isDesktopSize"
-      class="tabs">
+    <div v-if="isDesktopSize">
+    <div class="tabs">
       <button 
         v-for="(jsonData, index) in jsonData" 
-        :key="index" @click="selectedTab = index" 
+        :key="index" 
+        @click="selectedTab = index" 
         :class="{ active: selectedTab === index }">
           {{jsonData.title}}
       </button>
     </div>
     <div class="tab-content">
       <div v-html="jsonData[selectedTab].content"></div>
+    </div>
+  </div>
+  <div v-if="!isDesktopSize">
+    <div
+      class="accordion"
+      v-for="(jsonData, index) in jsonData" 
+      :key="index"
+      >
+      <button 
+      @click="toggleAccordion(index)" 
+      :class="{ active: activeSection === index }">
+        {{ jsonData.title }}
+      </button>
+      <div v-if="activeSection === index">
+        <div v-html="jsonData.content"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,14 +36,26 @@ import data from '../../static/data.json'
     export default {
       data() {
         return {
-          isDesktopSize: window.innerWidth > 1000,
           selectedTab: 0,
+          activeSection: 0,
           jsonData: data
-    }
-  }
+          }
+        },
+      methods: {
+        toggleAccordion(index) {
+          this.activeSection = this.activeSection === index ? null : index;
+        }
+      },
+      computed: {
+        isDesktopSize() {
+          return window.innerWidth > 1000
+        }
+      },
     }
 </script>
 
 <style lang="scss" scoped>
-
+.active {
+  background-color: greenyellow;
+}
 </style>
