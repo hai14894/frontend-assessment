c29e56da05e55d689effe58835cdc5972b10505e
Hai Ho <hohoanghai14894@gmail.com>
2023-02-07 11:36:55 +1100

    Add Card and CardButton components

---
+++ exercise1/src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <img src="../assets/400x300.png" />
+        <div>
+            {{ cardText }}
+        </div>
+       <CardButton 
+        msg="READ MORE"/>
+    </div>
+</template>
+
+<script>
+import CardButton from './CardButton.vue';
+export default {
+  components: { CardButton },
+  props: {
+    cardText: {
+                type: String,
+                required: true
+            },
+  }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ exercise1/src/components/CardButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="button">
+        {{ msg }}
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            msg: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.button{
+    font-weight: bold;
+    
+}
+</style>
